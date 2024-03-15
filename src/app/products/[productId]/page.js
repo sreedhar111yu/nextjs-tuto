ebff19c122428aaef98e765fde1871c92b3bb06b
sreedhar111yu <sreedharv36@gmail.com>
2024-03-15 19:55:44 +0530

    dynamic route completed

---
+++ src/app/products/[productId]/page.js
@@ -0,0 +1,14 @@
+export default function productId({params}){
+    return(
+
+        <>
+        <h1>PRODUCT IDS{params.productId}</h1>
+        </>
+    )
+}
+
+// next js react for the folder name of square brace in dynamic segment .  params id containe the parameter as
+//key value pairs in key of product id route have single parameter that is product id 
+
+//first created a folder in square [] and id with in the [].
+//then created page.js file inside the folder this allow map any route by url /poducts/id
