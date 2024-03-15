7f101747914b7a4d7474082351e2e5cdf44f2751
sreedhar111yu <sreedharv36@gmail.com>
2024-03-15 19:36:11 +0530

    dynamic route first step is complited

---
+++ src/app/products/page.js
@@ -0,0 +1,12 @@
+export default function productList(){
+    return(
+        <>
+        <h1>PRODUCTS LIST</h1>
+        <h2>PRODUCTS 1</h2>
+        <h2>PRODUCTS 2</h2>
+        <h2>PRODUCTS 3</h2>
+  
+         
+         </>
+    )
+}
